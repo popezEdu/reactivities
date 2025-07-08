8d053c63831cb15f4cbe0c16abffdde7c1be5e63
popezEdu <eduardo.lozu@gmail.com>
2025-07-08 16:49:29 -0400

    FE-Activity React Query

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,34 +1,49 @@
-import { Box, Container, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [activities, setActivities] = useState<IActivity[]>([]);
+  // const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     IActivity | undefined
   >(undefined);
   const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    // fetch("https://localhost:5001/api/activities")
-    // .then(response => response.json())
-    // .then(data => setActivities(data));
-    axios
-      .get<IActivity[]>("https://localhost:5001/api/activities")
-      .then((response) => {
-        setActivities(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching activities:", error);
-      });
+  // useEffect(() => {
+  //   // fetch("https://localhost:5001/api/activities")
+  //   // .then(response => response.json())
+  //   // .then(data => setActivities(data));
+  //   axios
+  //     .get<IActivity[]>("https://localhost:5001/api/activities")
+  //     .then((response) => {
+  //       setActivities(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching activities:", error);
+  //     });
 
-    return () => {};
-  }, []);
+  //   return () => {};
+  // }, []);
+
+  // Al implementar react-query, no es necesario el useEffect para obtener los datos
+  // Por lo que el useEffect será removido junto con el estado de activities
+  const { data: activities, isPending } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const response = await axios.get<IActivity[]>(
+        "https://localhost:5001/api/activities"
+      );
+      return response.data;
+    },
+    // Si se desea que se actualice cada cierto tiempo
+    // refetchInterval: 10000, // 10 segundos
+  });
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find((activity) => activity.id === id));
+    setSelectedActivity(activities!.find((activity) => activity.id === id));
   };
 
   const handleCancelSelectActivity = () => {
@@ -36,7 +51,6 @@ function App() {
   };
 
   const handleOpenForm = (id?: string) => {
-    console.log(id);
     if (id) {
       handleSelectActivity(id);
     } else {
@@ -50,41 +64,47 @@ function App() {
   };
 
   const handleSubmitForm = (activity: IActivity) => {
-    if (activity.id) {
-      setActivities([
-        ...activities.filter((a) => a.id !== activity.id),
-        activity,
-      ]);
-    } else {
-      activity.id = crypto.randomUUID();
-      setActivities([...activities, activity]);
-    }
-    setSelectedActivity(activity);
+    // if (activity.id) {
+    //   setActivities([
+    //     ...activities.filter((a) => a.id !== activity.id),
+    //     activity,
+    //   ]);
+    // } else {
+    //   activity.id = crypto.randomUUID();
+    //   setActivities([...activities, activity]);
+    // }
+    // setSelectedActivity(activity);
+    console.log(activity);
     setEditMode(false);
   };
 
   const handleDelete = (id: string) => {
-    setActivities(activities.filter((activity) => activity.id !== id));
+    // setActivities(activities.filter((activity) => activity.id !== id));
+    console.log(id);
     setSelectedActivity(undefined);
   };
 
   return (
-    <Box sx={{ bgcolor: "#eeeeee" }}>
+    <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       {/* Este componente elimina el margin CssBaseLine */}
       <CssBaseline />
       <NavBar openForm={handleOpenForm} />
       <Container maxWidth="xl" sx={{ marginTop: 1 }}>
-        <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSubmitForm}
-          deleteActivity={handleDelete}
-        />
+        {!activities && isPending ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          <ActivityDashboard
+            activities={activities}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            selectedActivity={selectedActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+            submitForm={handleSubmitForm}
+            deleteActivity={handleDelete}
+          />
+        )}
       </Container>
     </Box>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,16 @@ import "@fontsource/roboto/700.css";
 
 import "./app/layout/styles.css";
 import App from "./app/layout/App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools></ReactQueryDevtools>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
